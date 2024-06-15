35db93417e3fc1b30b9b5de59d209a5d41a5199d
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2024-06-15 12:10:41 +0300

    1

--- js/script.js
+++ js/script.js
@@ -13,26 +13,52 @@ window.onload = () => {
 
         $(clas.join()).click(function (event) {
             if (clas.indexOf("." + $(this).attr("class").substr(12, 14)) !== -1) {
-                clas.splice("." + $(this).attr("class").substr(12, 14), 1);
                 walked++
+                // console.log(walked);
+
 
                 if (walked % 2 == 0) {
+
                     zeros.push("." + $(this).attr("class").substr(12, 14));
-                    console.log("zeros = ", zeros);
+                    // console.log("zeros = ", zeros);
+                    for (let i = 0; i < crosses.length; i++) {
+
+
+                        if ("." + $(this).attr("class").substr(12, 14) == crosses[i]) {
+                            var a = '.CAImg.Img' + crosses[i].substr(1, 2);
+                            console.log(1);
+                            $(a).show();
+                            $(a).is(':visible')
+
+
+
+                            clas.splice("." + $(this).attr("class").substr(12, 14), 1);
+
+                        }
+                    }
                 } else {
+
                     crosses.push("." + $(this).attr("class").substr(12, 14));
-                    console.log("crosses = ", crosses);
+                    // console.log("crosses = ", crosses);
+                    for (let i = 0; i < zeros.length; i++) {
+
+
+                        if ("." + $(this).attr("class").substr(12, 14) == zeros[i]) {
+                            var a = '.ZAImg.Img' + zeros[i].substr(1, 2);
+
+                            // $(a).show();
+                            $(a).is(':visible')
+
+
+                            clas.splice("." + $(this).attr("class").substr(12, 14), 1);
+
+                        }
+                    }
                 }
 
-                for (let i = 0; i < crosses.length; i++) {
-                    var a = '.CAImg.Img'+crosses[i].substr(1, 2);
-                    $(a).show();
-                }
 
-                for (let i = 0; i < zeros.length; i++) {
-                    var a = '.ZAImg.Img'+zeros[i].substr(1, 2);
-                    $(a).show();
-                }
+
+
 
 
                 // if(crosses.indexOf('.AA') != -1 && crosses.indexOf('.BA') != -1 && crosses.indexOf('.CA') != -1){
