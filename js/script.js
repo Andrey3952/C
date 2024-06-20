83b300eceaec1c62bfe9cdb0a28e6dc9e55be227
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2024-06-20 13:45:02 +0300

    a

--- js/script.js
+++ js/script.js
@@ -1,65 +1,74 @@
 window.onload = () => {
     $(document).ready(function () {
-        // $('.ZAImg.ImgAA').show();
+            // $('.ZAImg.ImgAA').show();
 
-        var walked = 0;
-        var clas = ['.AA', '.AB', '.AC',
-            '.BA', '.BB', '.BC',
-            '.CA', '.CB', '.CC'];
-        var crosses = [];
-        var zeros = [];
+            var walked = 0;
+            var clas = ['.AA', '.AB', '.AC',
+                '.BA', '.BB', '.BC',
+                '.CA', '.CB', '.CC'];
+            var crosses = [];
+            var zeros = [];
+            var game = 0;
 
 
             $(clas.join()).click(function (event) {
-                console.log(walked);
+
                 var thisClassString = clas[clas.indexOf("." + $(this).attr("class").substr(12, 14))];
                 var thisClassInt = clas.indexOf("." + $(this).attr("class").substr(12, 14));
 
+                if (game === 0) {
+                    console.log(game);
+                    if (thisClassInt !== -1) {
+                        walked++
 
-                if (thisClassInt !== -1) {
-                    walked++
+                        if (walked % 2 !== 0) {
 
-                    if (walked % 2 === 0) {
-
-                        crosses.push(thisClassString);
+                            crosses.push(thisClassString);
 
 
-                        for (let i = 0; i < crosses.length; i++) {
+                            for (let i = 0; i < crosses.length; i++) {
 
-                            if (thisClassString === crosses[i]) {
-                                var a = '.CAImg.Img' + crosses[i].substr(1, 2);
-                                $(a).show();
+                                if (thisClassString === crosses[i]) {
+                                    var a = '.CAImg.Img' + crosses[i].substr(1, 2);
+                                    $(a).show();
 
-                                clas.splice(thisClassInt, 1);
+                                    clas.splice(thisClassInt, 1);
 
+                                }
+                            }
+
+
+                        } else {
+                            zeros.push(thisClassString);
+
+
+                            for (let i = 0; i < zeros.length; i++) {
+
+                                if (thisClassString === zeros[i]) {
+                                    var a = '.ZAImg.Img' + zeros[i].substr(1, 2);
+                                    $(a).show();
+
+                                    clas.splice(thisClassInt, 1);
+
+                                }
                             }
                         }
 
-
-                    } else {
-                        zeros.push(thisClassString);
-
-
-                        for (let i = 0; i < zeros.length; i++) {
-
-                            if (thisClassString === zeros[i]) {
-                                var a = '.ZAImg.Img' + zeros[i].substr(1, 2);
-                                $(a).show();
-
-                                clas.splice(thisClassInt, 1);
-
-                            }
-                        }
                     }
-
                 }
+
+                if (zeros.indexOf(".AA") !== -1 && zeros.indexOf(".BA") !== -1 && zeros.indexOf(".CA") !== -1) {
+                    $(".AImg").show();
+                    game = 1;
+                }
+
 
             });
 
         }
     )
-        ;
-    }
+    ;
+}
 
 
 
