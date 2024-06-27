193fc93bb4fda1c36f427c3330faaf0e7429c0a1
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2024-06-27 13:35:03 +0300

    q

--- js/script.js
+++ js/script.js
@@ -1,73 +1,129 @@
 window.onload = () => {
     $(document).ready(function () {
-            // $('.ZAImg.ImgAA').show();
+        // $('.ZAImg.ImgAA').show();
 
-            var walked = 0;
-            var clas = ['.AA', '.AB', '.AC',
-                '.BA', '.BB', '.BC',
-                '.CA', '.CB', '.CC'];
-            var crosses = [];
-            var zeros = [];
-            var game = 0;
-
-
-            $(clas.join()).click(function (event) {
-
-                var thisClassString = clas[clas.indexOf("." + $(this).attr("class").substr(12, 14))];
-                var thisClassInt = clas.indexOf("." + $(this).attr("class").substr(12, 14));
-
-                if (game === 0) {
-                    console.log(game);
-                    if (thisClassInt !== -1) {
-                        walked++
-
-                        if (walked % 2 !== 0) {
-
-                            crosses.push(thisClassString);
+        var walked = 0;
+        var clas = ['.AA', '.AB', '.AC',
+            '.BA', '.BB', '.BC',
+            '.CA', '.CB', '.CC'];
+        var crosses = [];
+        var zeros = [];
+        var game = 0;
 
 
-                            for (let i = 0; i < crosses.length; i++) {
+        $(clas.join()).click(function (event) {
 
-                                if (thisClassString === crosses[i]) {
-                                    var a = '.CAImg.Img' + crosses[i].substr(1, 2);
-                                    $(a).show();
+            var thisClassString = clas[clas.indexOf("." + $(this).attr("class").substr(12, 14))];
+            var thisClassInt = clas.indexOf("." + $(this).attr("class").substr(12, 14));
 
-                                    clas.splice(thisClassInt, 1);
+            if (game === 0) {
+                console.log(game);
+                if (thisClassInt !== -1) {
+                    walked++
 
-                                }
-                            }
+                    if (walked % 2 !== 0) {
+
+                        crosses.push(thisClassString);
 
 
-                        } else {
-                            zeros.push(thisClassString);
+                        for (let i = 0; i < crosses.length; i++) {
+
+                            if (thisClassString === crosses[i]) {
+                                var a = '.CAImg.Img' + crosses[i].substr(1, 2);
+
+                                $(a).addClass('Crosses');
 
 
-                            for (let i = 0; i < zeros.length; i++) {
+                                clas.splice(thisClassInt, 1);
 
-                                if (thisClassString === zeros[i]) {
-                                    var a = '.ZAImg.Img' + zeros[i].substr(1, 2);
-                                    $(a).show();
-
-                                    clas.splice(thisClassInt, 1);
-
-                                }
                             }
                         }
 
+
+                    } else {
+                        zeros.push(thisClassString);
+
+
+                        for (let i = 0; i < zeros.length; i++) {
+
+                            if (thisClassString === zeros[i]) {
+                                var a = '.ZAImg.Img' + zeros[i].substr(1, 2);
+                                $(a).addClass('Zero');
+
+
+                                clas.splice(thisClassInt, 1);
+
+                            }
+                        }
                     }
+
                 }
+            }
 
-                if (zeros.indexOf(".AA") !== -1 && zeros.indexOf(".BA") !== -1 && zeros.indexOf(".CA") !== -1) {
-                    $(".AImg").show();
-                    game = 1;
-                }
+            // if (zeros.indexOf(".AA") !== -1 && zeros.indexOf(".BA") !== -1 && zeros.indexOf(".CA") !== -1 || crosses.indexOf(".AA") !== -1 && crosses.indexOf(".BA") !== -1 && crosses.indexOf(".CA") !== -1) {
+
+            //     $(".AImg").css("top", "0");
+            //     $(".AImg").css("left", "50%");
+            //     $(".AImg").show();
+            //     game = 1;
+            // }
+
+            // else if (zeros.indexOf(".AB") !== -1 && zeros.indexOf(".BB") !== -1 && zeros.indexOf(".CB") !== -1 || crosses.indexOf(".AB") !== -1 && crosses.indexOf(".BB") !== -1 && crosses.indexOf(".CB") !== -1) {
+            //     $(".AImg").css("top", "0");
+            //     $(".AImg").css("left", "150%");
+            //     $(".AImg").show();
+            //     game = 1;
+            // }
+
+            // else if (zeros.indexOf(".AC") !== -1 && zeros.indexOf(".BC") !== -1 && zeros.indexOf(".CC") !== -1 || crosses.indexOf(".AC") !== -1 && crosses.indexOf(".BC") !== -1 && crosses.indexOf(".CC") !== -1) {
+            //     $(".AImg").css("top", "0");
+            //     $(".AImg").css("left", "250%");
+            //     $(".AImg").show();
+            //     game = 1;
+            // }
+
+            // else if (zeros.indexOf(".CA") !== -1 && zeros.indexOf(".CB") !== -1 && zeros.indexOf(".CC") !== -1 || crosses.indexOf(".CA") !== -1 && crosses.indexOf(".CB") !== -1 && crosses.indexOf(".CC") !== -1) {
+            //     $(".AImgD").css("top", "34.3%");
+            //     $(".AImgD").css("left", "150%");
+            //     $(".AImgD").show();
+            //     game = 1;
+            // }
+
+            // else if (zeros.indexOf(".AA") !== -1 && zeros.indexOf(".AB") !== -1 && zeros.indexOf(".AC") !== -1 || crosses.indexOf(".AA") !== -1 && crosses.indexOf(".AB") !== -1 && crosses.indexOf(".AC") !== -1) {
+            //     $(".AImgD").css("top", "-34.3%");
+            //     $(".AImgD").css("left", "150%");
+            //     $(".AImgD").show();
+            //     game = 1;
+            // }
+
+            // else if (zeros.indexOf(".BA") !== -1 && zeros.indexOf(".BB") !== -1 && zeros.indexOf(".BC") !== -1 || crosses.indexOf(".BA") !== -1 && crosses.indexOf(".BB") !== -1 && crosses.indexOf(".BC") !== -1) {
+            //     $(".AImgD").css("top", "0.3%");
+            //     $(".AImgD").css("left", "150%");
+            //     $(".AImgD").show();
+            //     game = 1;
+            // }
+
+            // else if ( zeros.indexOf(".AA") !== -1 && zeros.indexOf(".BB") !== -1 && zeros.indexOf(".CC") !== -1 || crosses.indexOf(".AA") !== -1 && crosses.indexOf(".BB") !== -1 && crosses.indexOf(".CC") !== -1) {
+            //     $(".AImgS").show();
+            //     game = 1;
+            // }
+
+            // else if ( zeros.indexOf(".CA") !== -1 && zeros.indexOf(".BB") !== -1 && zeros.indexOf(".AC") !== -1 || crosses.indexOf(".CA") !== -1 && crosses.indexOf(".BB") !== -1 && crosses.indexOf(".AC") !== -1) {
+            //     $(".AImgMS").show();
+            //     game = 1;
+            // }
 
 
-            });
 
-        }
+    
+
+
+
+        });
+
+    }
     )
-    ;
+        ;
 }
 
 
